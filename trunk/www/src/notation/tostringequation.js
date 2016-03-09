a2b4c0e2e2d09b9d3650d0c77da4a8b838289c02
Rodrigo Azevedo <rodrigo_4zevedo@hotmail.com>
2016-03-09 00:45:56 -0300

    Created non power expressions function

--- trunk/www/src/notation/tostringequation.js
+++ trunk/www/src/notation/tostringequation.js
@@ -1,4 +1,10 @@
-function stringEquation( node )
+function stringEquation(node)
+{
+  var n = NPE_transform(node);
+  return stringEquation_rec(n);
+}
+
+function stringEquation_rec( node )
 {
   var ret = 0;
   var left = 0;
@@ -18,12 +24,12 @@ function stringEquation( node )
             if(signal(node.children[i]) && i > 0) {
               group += "+";
             }
-            group += stringEquation(node.children[i]);
+            group += stringEquation_rec(node.children[i]);
           }
           ret = group;
           break;
         case OP_SUB:
-          ret = stringEquation( node.children[0] ) + "-" + stringEquation( node.children[1] );
+          ret = stringEquation_rec( node.children[0] ) + "-" + stringEquation_rec( node.children[1] );
           break;
         case OP_DIV:
           ret = stringEquationParen( node.children[0] ) + "/" + stringEquationParen( node.children[1] );
@@ -36,18 +42,18 @@ function stringEquation( node )
               first = "-";
             else
             {
-              var newpush = node.children[i].type == NODE_OP && (node.children[i].value == OP_ADD || node.children[i].value == OP_SUB) ? "(" + stringEquation( node.children[i] ) + ")" : stringEquation( node.children[i] );
+              var newpush = node.children[i].type == NODE_OP && (node.children[i].value == OP_ADD || node.children[i].value == OP_SUB) ? "(" + stringEquation_rec( node.children[i] ) + ")" : stringEquation_rec( node.children[i] );
               ChildrenTex.push(newpush);
             }
           }
           ret = first + ChildrenTex.join("*");
           break;
         case OP_NEG:
-          ret = node.children[0].type == NODE_OP ? "(-" + stringEquation( node.children[0] ) + ")" : "-"+ stringEquationParen( node.children[0] );
+          ret = node.children[0].type == NODE_OP ? "(-" + stringEquation_rec( node.children[0] ) + ")" : "-"+ stringEquationParen( node.children[0] );
           break;
         case OP_POW:
-          left = node.children[0].type == NODE_OP ? "(" + stringEquation( node.children[0] ) + ")" : stringEquation( node.children[0] );
-          right = node.children[1].type == NODE_OP ? "(" + stringEquation( node.children[1] ) + ")" : stringEquation( node.children[1] );
+          left = node.children[0].type == NODE_OP ? "(" + stringEquation_rec( node.children[0] ) + ")" : stringEquation_rec( node.children[0] );
+          right = node.children[1].type == NODE_OP ? "(" + stringEquation_rec( node.children[1] ) + ")" : stringEquation_rec( node.children[1] );
           ret = left +"^"+ right;
           break;
       }
@@ -57,88 +63,88 @@ function stringEquation( node )
       switch( node.value )
       {
         case FUNC_SIN:
-          ret = "sin(" + stringEquation( node.children[0] ) + ")";
+          ret = "sin(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_COS:
-          ret = "cos(" + stringEquation( node.children[0] ) + ")";
+          ret = "cos(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_ASINH:
-          ret = "asinh(" + stringEquation( node.children[0] ) + ")";
+          ret = "asinh(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_SINH:
-          ret = "sinh(" + stringEquation( node.children[0] ) + ")";
+          ret = "sinh(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_ASIN:
-          ret = "asin(" + stringEquation( node.children[0] ) + ")";
+          ret = "asin(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_ACOSH:
-          ret = "acosh(" + stringEquation( node.children[0] ) + ")";
+          ret = "acosh(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_COSH:
-          ret = "cosh(" + stringEquation( node.children[0] ) + ")";
+          ret = "cosh(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_ACOS:
-          ret = "acos(" + stringEquation( node.children[0] ) + ")";
+          ret = "acos(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_ATANH:
-          ret = "atanh(" + stringEquation( node.children[0] ) + ")";
+          ret = "atanh(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_TANH:
-          ret = "tanh(" + stringEquation( node.children[0] ) + ")";
+          ret = "tanh(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_ATAN:
-          ret = "atan(" + stringEquation( node.children[0] ) + ")";
+          ret = "atan(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_TAN:
-          ret = "tan(" + stringEquation( node.children[0] ) + ")";
+          ret = "tan(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_ASECH:
-          ret = "asech(" + stringEquation( node.children[0] ) + ")";
+          ret = "asech(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_SECH:
-          ret = "sech(" + stringEquation( node.children[0] ) + ")";
+          ret = "sech(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_ASEC:
-          ret = "asec(" + stringEquation( node.children[0] ) + ")";
+          ret = "asec(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_SEC:
-          ret = "sec(" + stringEquation( node.children[0] ) + ")";
+          ret = "sec(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_ACSCH:
-          ret = "acsch(" + stringEquation( node.children[0] ) + ")";
+          ret = "acsch(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_CSCH:
-          ret = "csch(" + stringEquation( node.children[0] ) + ")";
+          ret = "csch(" + stringEquation_rec( node.children[0] ) + ")";
           break;
 		    case FUNC_ACSC:
-          ret = "acsc(" + stringEquation( node.children[0] ) + ")";
+          ret = "acsc(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_CSC:
-          ret = "csc(" + stringEquation( node.children[0] ) + ")";
+          ret = "csc(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_ACOTH:
-          ret = "acoth(" + stringEquation( node.children[0] ) + ")";
+          ret = "acoth(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_COTH:
-          ret = "coth(" + stringEquation( node.children[0] ) + ")";
+          ret = "coth(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_ACOT:
-          ret = "acot(" + stringEquation( node.children[0] ) + ")";
+          ret = "acot(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_COT:
-          ret = "cot(" + stringEquation( node.children[0] ) + ")";
+          ret = "cot(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_SQRT:
-          ret = "sqrt(" + stringEquation( node.children[0] ) + ")";
+          ret = "sqrt(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_EXP:
-          ret = "exp(" + stringEquation( node.children[0] ) + ")";
+          ret = "exp(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_NLOG:
-          ret = "log(" + stringEquation( node.children[0] ) + ")";
+          ret = "log(" + stringEquation_rec( node.children[0] ) + ")";
           break;
         case FUNC_BLOG:
-          ret = "log" + stringEquation( node.children[0] ) + "(" + stringEquation( node.children[1] ) + ")";
+          ret = "log" + stringEquation_rec( node.children[0] ) + "(" + stringEquation_rec( node.children[1] ) + ")";
           break;
       }
       break;
@@ -158,7 +164,7 @@ function stringEquation( node )
 function stringEquationParen( node )
 {
   if(node.type == NODE_OP)
-    return "(" + stringEquation( node ) + ")";
+    return "(" + stringEquation_rec( node ) + ")";
   else
-    return stringEquation( node );
+    return stringEquation_rec( node );
 }
--- trunk/www/src/notation/totex.js
+++ trunk/www/src/notation/totex.js
@@ -1,4 +1,10 @@
-function toTex( node )
+function toTex(node)
+{
+  var n = NPE_transform(node);
+  return toTex_rec(n);
+}
+
+function toTex_rec( node )
 {
   var ret = 0;
   var left = 0;
@@ -18,15 +24,15 @@ function toTex( node )
             if(signal(node.children[i]) && i > 0) {
               group += "+";
             }
-            group += toTex(node.children[i]);
+            group += toTex_rec(node.children[i]);
           }
           ret = group;
           break;
         case OP_SUB:
-          ret = toTex( node.children[0] ) + "-" + toTexParenADDSUB( node.children[1] );
+          ret = toTex_rec( node.children[0] ) + "-" + toTex_recParenADDSUB( node.children[1] );
           break;
         case OP_DIV:
-          ret = "{" + toTex( node.children[0] ) + "}/{" + toTex( node.children[1] ) +"}";
+          ret = "{" + toTex_rec( node.children[0] ) + "}/{" + toTex_rec( node.children[1] ) +"}";
           break;
         case OP_MUL:
           var ChildrenTex = new Array();
@@ -49,7 +55,7 @@ function toTex( node )
                   ChildrenTex.push("\\,");
                 }
               }
-              var newpush = node.children[i].type == NODE_OP && (node.children[i].value == OP_ADD || node.children[i].value == OP_SUB) ? "(" + toTex( node.children[i] ) + ")" : toTex( node.children[i] );
+              var newpush = node.children[i].type == NODE_OP && (node.children[i].value == OP_ADD || node.children[i].value == OP_SUB) ? "(" + toTex_rec( node.children[i] ) + ")" : toTex_rec( node.children[i] );
               ChildrenTex.push(newpush);
             }
           }
@@ -57,29 +63,29 @@ function toTex( node )
           // Need to develop terms grouping before release this function
           // if(node.children[1].type == NODE_INT)
           // {
-          // 	left = node.children[0].type == NODE_OP && (node.children[0].value == OP_ADD || node.children[0].value == OP_SUB) ? "(" + toTex( node.children[0] ) + ")" : toTex( node.children[0] );
-          //   ret = left + "·" + toTex( node.children[1] );
+          // 	left = node.children[0].type == NODE_OP && (node.children[0].value == OP_ADD || node.children[0].value == OP_SUB) ? "(" + toTex_rec( node.children[0] ) + ")" : toTex_rec( node.children[0] );
+          //   ret = left + "·" + toTex_rec( node.children[1] );
           // }else if(node.children[1].type == NODE_SYM){
-          //   left = node.children[0].type == NODE_OP && (node.children[0].value == OP_ADD || node.children[0].value == OP_SUB) ? "(" + toTex( node.children[0] ) + ")" : toTex( node.children[0] );
-          //   ret = left + "\\," + toTex( node.children[1] );
+          //   left = node.children[0].type == NODE_OP && (node.children[0].value == OP_ADD || node.children[0].value == OP_SUB) ? "(" + toTex_rec( node.children[0] ) + ")" : toTex_rec( node.children[0] );
+          //   ret = left + "\\," + toTex_rec( node.children[1] );
           // }else{
-          //   left = node.children[0].type == NODE_OP && (node.children[0].value == OP_ADD || node.children[0].value == OP_SUB) ? "(" + toTex( node.children[0] ) + ")" : toTex( node.children[0] );
-          //   right = node.children[1].type == NODE_OP && (node.children[1].value == OP_ADD || node.children[1].value == OP_SUB) ? "(" + toTex( node.children[1] ) + ")" : toTex( node.children[1] );
+          //   left = node.children[0].type == NODE_OP && (node.children[0].value == OP_ADD || node.children[0].value == OP_SUB) ? "(" + toTex_rec( node.children[0] ) + ")" : toTex_rec( node.children[0] );
+          //   right = node.children[1].type == NODE_OP && (node.children[1].value == OP_ADD || node.children[1].value == OP_SUB) ? "(" + toTex_rec( node.children[1] ) + ")" : toTex_rec( node.children[1] );
           //   ret = left + "\\," + right;
           // }
           break;
         case OP_NEG:
-          //ret = node.children[0].type == NODE_OP ? "(-" + toTex( node.children[0] ) + ")" : "-"+ toTex( node.children[0] );
-          ret = "-" + toTexParenADDSUB( node.children[0] );
+          //ret = node.children[0].type == NODE_OP ? "(-" + toTex_rec( node.children[0] ) + ")" : "-"+ toTex_rec( node.children[0] );
+          ret = "-" + toTex_recParenADDSUB( node.children[0] );
           break;
         case OP_POW:
           if(node.children[0].type == NODE_FUNC)
           {
-            ret = toTexPowerFunc(node.children[0], node.children[1]);
+            ret = toTex_recPowerFunc(node.children[0], node.children[1]);
           }
           else {
-            left = node.children[0].type == NODE_OP ? "(" + toTex( node.children[0] ) + ")" : toTex( node.children[0] );
-            ret = left +"^{"+ toTex(node.children[1]) +"}";
+            left = node.children[0].type == NODE_OP ? "(" + toTex_rec( node.children[0] ) + ")" : toTex_rec( node.children[0] );
+            ret = left +"^{"+ toTex_rec(node.children[1]) +"}";
           }
           break;
       }
@@ -89,88 +95,88 @@ function toTex( node )
       switch( node.value )
       {
         case FUNC_SIN:
-          ret = "sin(" + toTex( node.children[0] ) + ")";
+          ret = "sin(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_COS:
-          ret = "cos(" + toTex( node.children[0] ) + ")";
+          ret = "cos(" + toTex_rec( node.children[0] ) + ")";
           break;
          case FUNC_ASINH:
-          ret = "asinh(" + toTex( node.children[0] ) + ")";
+          ret = "asinh(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_SINH:
-          ret = "sinh(" + toTex( node.children[0] ) + ")";
+          ret = "sinh(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_ASIN:
-          ret = "asin(" + toTex( node.children[0] ) + ")";
+          ret = "asin(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_ACOSH:
-          ret = "acosh(" + toTex( node.children[0] ) + ")";
+          ret = "acosh(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_COSH:
-          ret = "cosh(" + toTex( node.children[0] ) + ")";
+          ret = "cosh(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_ACOS:
-          ret = "acos(" + toTex( node.children[0] ) + ")";
+          ret = "acos(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_ATANH:
-          ret = "atanh(" + toTex( node.children[0] ) + ")";
+          ret = "atanh(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_TANH:
-          ret = "tanh(" + toTex( node.children[0] ) + ")";
+          ret = "tanh(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_ATAN:
-          ret = "atan(" + toTex( node.children[0] ) + ")";
+          ret = "atan(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_TAN:
-          ret = "tan(" + toTex( node.children[0] ) + ")";
+          ret = "tan(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_ASECH:
-          ret = "asech(" + toTex( node.children[0] ) + ")";
+          ret = "asech(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_SECH:
-          ret = "sech(" + toTex( node.children[0] ) + ")";
+          ret = "sech(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_ASEC:
-          ret = "asec(" + toTex( node.children[0] ) + ")";
+          ret = "asec(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_SEC:
-          ret = "sec(" + toTex( node.children[0] ) + ")";
+          ret = "sec(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_ACSCH:
-          ret = "acsch(" + toTex( node.children[0] ) + ")";
+          ret = "acsch(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_CSCH:
-          ret = "csch(" + toTex( node.children[0] ) + ")";
+          ret = "csch(" + toTex_rec( node.children[0] ) + ")";
           break;
 		    case FUNC_ACSC:
-          ret = "acsc(" + toTex( node.children[0] ) + ")";
+          ret = "acsc(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_CSC:
-          ret = "csc(" + toTex( node.children[0] ) + ")";
+          ret = "csc(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_ACOTH:
-          ret = "acoth(" + toTex( node.children[0] ) + ")";
+          ret = "acoth(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_COTH:
-          ret = "coth(" + toTex( node.children[0] ) + ")";
+          ret = "coth(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_ACOT:
-          ret = "acot(" + toTex( node.children[0] ) + ")";
+          ret = "acot(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_COT:
-          ret = "cot(" + toTex( node.children[0] ) + ")";
+          ret = "cot(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_SQRT:
-          ret = "{√{" + toTex( node.children[0] ) + "}}";
+          ret = "{√{" + toTex_rec( node.children[0] ) + "}}";
           break;
         case FUNC_EXP:
-          ret = "exp(" + toTex( node.children[0] ) + ")";
+          ret = "exp(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_NLOG:
-          ret = "log(" + toTex( node.children[0] ) + ")";
+          ret = "log(" + toTex_rec( node.children[0] ) + ")";
           break;
         case FUNC_BLOG:
-          ret = "log_{" + toTex( node.children[0] ) + "}(" + toTex( node.children[1] ) + ")";
+          ret = "log_{" + toTex_rec( node.children[0] ) + "}(" + toTex_rec( node.children[1] ) + ")";
           break;
       }
       break;
@@ -187,102 +193,102 @@ function toTex( node )
   return ret;
 }
 
-function toTexParenADDSUB( node )
+function toTex_recParenADDSUB( node )
 {
   if(kind(node) == OP_ADD || kind(node) == OP_SUB)
-    return "(" + toTex( node ) + ")";
+    return "(" + toTex_rec( node ) + ")";
   else
-    return toTex( node );
+    return toTex_rec( node );
 }
 
-function toTexPowerFunc(node, power)
+function toTex_recPowerFunc(node, power)
 {
     var ret = 0;
     switch( node.value )
     {
       case FUNC_SIN:
-        ret = "{sin}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{sin}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_COS:
-        ret = "{cos}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{cos}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
        case FUNC_ASINH:
-        ret = "{asinh}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{asinh}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_SINH:
-        ret = "{sinh}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{sinh}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_ASIN:
-        ret = "{asin}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{asin}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_ACOSH:
-        ret = "{acosh}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{acosh}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_COSH:
-        ret = "{cosh}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{cosh}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_ACOS:
-        ret = "{acos}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{acos}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_ATANH:
-        ret = "{atanh}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{atanh}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_TANH:
-        ret = "{tanh}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{tanh}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_ATAN:
-        ret = "{atan}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{atan}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_TAN:
-        ret = "{tan}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{tan}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_ASECH:
-        ret = "{asech}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{asech}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_SECH:
-        ret = "{sech}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{sech}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_ASEC:
-        ret = "{asec}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{asec}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_SEC:
-        ret = "{sec}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{sec}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_ACSCH:
-        ret = "{acsch}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{acsch}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_CSCH:
-        ret = "{csch}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{csch}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_ACSC:
-        ret = "{acsc}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{acsc}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_CSC:
-        ret = "{csc}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{csc}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_ACOTH:
-        ret = "{acoth}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{acoth}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_COTH:
-        ret = "{coth}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{coth}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_ACOT:
-        ret = "{acot}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{acot}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_COT:
-        ret = "{cot}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{cot}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_SQRT:
-        ret = "{√{" + toTex( node.children[0] ) + "}}";
+        ret = "{√{" + toTex_rec( node.children[0] ) + "}}";
         break;
       case FUNC_EXP:
-        ret = "{exp}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{exp}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_NLOG:
-        ret = "{log}^{" + toTex(power) + "}(" + toTex( node.children[0] ) + ")";
+        ret = "{log}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[0] ) + ")";
         break;
       case FUNC_BLOG:
-        ret = "{log}_{" + toTex( node.children[0] ) + "}^{" + toTex(power) + "}(" + toTex( node.children[1] ) + ")";
+        ret = "{log}_{" + toTex_rec( node.children[0] ) + "}^{" + toTex_rec(power) + "}(" + toTex_rec( node.children[1] ) + ")";
         break;
     }
     return ret;
